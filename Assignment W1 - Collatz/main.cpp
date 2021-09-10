b679dfd2c854962376c88197297c0eab5923469c
Mattia Peiretti <mattia.peiretti@gmail.com>
2021-09-10 19:15:36 +0200

    Collatz assignment completed

---
+++ Assignment W1 - Collatz/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int findNextCollatzSequence(int currentNumberOfSequence) {
+    // Checking if the first number is even
+    if (currentNumberOfSequence%2 == 0) currentNumberOfSequence = currentNumberOfSequence / 2;
+    // Number is odd
+    else currentNumberOfSequence = (currentNumberOfSequence * 3) + 1;
+    
+    return currentNumberOfSequence;
+}
+
+int main() {
+    const int MAX_NUMBER_OF_ONES = 3;
+    int currentNumberOfSequence;
+    int oneCounter = 0;
+
+    std::cout << "Please, enter the first number of the sequence: " << std::endl;
+    std::cin >> currentNumberOfSequence;
+    if(currentNumberOfSequence < 0) {
+        std::cout << "Illegal, only positive numbers are allowed!";
+        return -1;
+    }
+
+    std::cout << currentNumberOfSequence << " ";
+
+    while (oneCounter <= MAX_NUMBER_OF_ONES) {
+        currentNumberOfSequence = findNextCollatzSequence(currentNumberOfSequence);
+        std::cout << currentNumberOfSequence << " ";
+        if (currentNumberOfSequence == 1)
+            oneCounter++;
+    }
+
+    return 0;
+}
