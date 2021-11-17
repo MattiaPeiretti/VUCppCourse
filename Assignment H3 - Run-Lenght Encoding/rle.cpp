89d697fa5a86d15286cfafd0e028ce90a1450b4b
Mattia Peiretti <mattia.peiretti@gmail.com>
2021-11-18 00:02:37 +0100

    Progess on H3 (Check codegrade for fixes)

--- Assignment H3 - Run-Lenght Encoding/rle.cpp
+++ Assignment H3 - Run-Lenght Encoding/rle.cpp
@@ -7,18 +7,52 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <regex>
+
+#define LOG(X) std::cout << X << std::endl;
+#define LOGV(x)                       \
+    std::cout << "( ";                \
+    for (auto &element : x)           \
+        std::cout << element << ", "; \
+    std::cout << ")" << std::endl;
 
 int main()
 {
     std::string rawData;
     std::vector<char> characters;
-    std::vector<int> charactersAmount;
+    std::vector<int> runLengths;
 
     std::cout << "Enter the data to be compressed: ";
     std::getline(std::cin, rawData);
 
-    if (rawData.find_first_not_of("abcdefghijklmnopqrstuvwxyz") != std::string::npos)
+    if (!std::regex_match(rawData, std::regex("^[a-z]+$")))
+    {
+        std::cout << "error: invalid input" << std::endl;
         return -1;
+    }
+
+    char currentCharacter;
+    int characterPosition = -1;
+
+    for (std::string::size_type i = 0; i < rawData.size(); i++)
+    {
+        currentCharacter = rawData[i];
+        if (currentCharacter != rawData[i - 1])
+        {
+            characters.push_back(currentCharacter);
+            runLengths.push_back(0);
+            characterPosition++;
+        }
+
+        runLengths.at(characterPosition) = runLengths.at(characterPosition) + 1;
+    }
+
+    std::cout << "The compressed data is: ";
+
+    for (int i = 0; i <= characters.size() - 1; i++)
+    {
+        std::cout << runLengths.at(i) << characters.at(i);
+    }
 
     return 0;
 }
