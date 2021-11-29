c18cca14555a2c3089dfa6020f75c81826087eb6
Mattia Peiretti <mattia.peiretti@gmail.com>
2021-11-29 01:49:33 +0100

    Working H4

--- Assignment H4 - Number of Queens/Queens.cpp
+++ Assignment H4 - Number of Queens/Queens.cpp
@@ -4,10 +4,7 @@
 //  Assignment H4 - N of Queens
 // ============================================
 
-#define LOG(X) std::cout << X << std::endl;
-
 #include <iostream>
-#include <string>
 #include <vector>
 
 void printChessBoard(std::vector<std::vector<bool>> &board)
@@ -27,9 +24,9 @@ void printChessBoard(std::vector<std::vector<bool>> &board)
         }
         std::cout << std::endl;
     }
+    std::cout << std::endl;
 }
 
-// Check to see if there is any conflict with previously placed queens
 bool checkValidPosition(std::vector<std::vector<bool>> &board, int col, int row)
 {
     //Checking horizontally and vertically
@@ -72,38 +69,41 @@ bool checkValidPosition(std::vector<std::vector<bool>> &board, int col, int row)
     int currentX1 = d1BeginX, currentY1 = d1BeginY;
     int currentX2 = d2BeginX, currentY2 = d2BeginY;
 
+    // Checking the left-to-right diagonal
     while (true)
     {
-        if (currentX1 >= board.size(), currentY1 >= board.size())
-            break;
-        if (currentX2 >= board.size(), currentY2 >= board.size())
+        if (currentX1 >= board.size() || currentY1 >= board.size())
             break;
 
         if (board.at(currentY1).at(currentX1))
         {
-            return false;
-        }
-        if (board.at(currentY2).at(currentX2))
-        {
-            return false;
+            return 0;
         }
 
         currentX1++;
         currentY1++;
+    }
+
+    // Checking the right-to-left diagonal
+    while (true)
+    {
+        if (currentX2 < 0 || currentY2 >= board.size())
+            break;
+        if (board.at(currentY2).at(currentX2))
+        {
+            return 0;
+        }
         currentX2--;
-        currentY2--;
+        currentY2++;
     }
 
     return true;
 }
 
-int placedQueens = 0;
 bool placeQueens(int N, std::vector<std::vector<bool>> &board, int row)
 {
     if (row >= N)
         return 1;
-    printChessBoard(board);
-    std::cin.get();
 
     for (int col = 0; col < board.size(); col++)
     {
@@ -111,36 +111,29 @@ bool placeQueens(int N, std::vector<std::vector<bool>> &board, int row)
         {
             board.at(row).at(col) = true;
             if (placeQueens(N, board, row + 1))
-                return true;
+                return 1;
+
             board.at(row).at(col) = false;
         }
     }
-
     return 0;
-}
-
-int parseUserInput()
-{
-    int numberOfQueens;
-    std::cout << "How many queens to place on the board? ";
-    std::cin >> numberOfQueens;
-    if (std::cin.fail())
-        throw std::runtime_error("error: invalid input");
-    if (!numberOfQueens >= 1)
-        throw std::runtime_error("error: invalid input");
-    return numberOfQueens;
 }
 
 int main()
 {
-
     int numberOfQueens = 0;
     int chessBoardSize = 0;
     bool result;
 
     try
     {
-        chessBoardSize = numberOfQueens = parseUserInput();
+        std::cout << "How many queens to place on the board? ";
+        std::cin >> numberOfQueens;
+        if (std::cin.fail())
+            throw std::runtime_error("error: invalid input");
+        if (!numberOfQueens >= 1)
+            throw std::runtime_error("error: invalid input");
+        chessBoardSize = numberOfQueens;
     }
     catch (std::runtime_error &excpt)
     {
@@ -149,10 +142,20 @@ int main()
     }
 
     std::vector<std::vector<bool>> chessBoard(chessBoardSize, std::vector<bool>(chessBoardSize, false));
-    // result = placeQueens(numberOfQueens, chessBoard, 1);
 
-    placeQueens(numberOfQueens, chessBoard, 0);
-    printChessBoard(chessBoard);
+    if (placeQueens(numberOfQueens, chessBoard, 0))
+    {
+        printChessBoard(chessBoard);
+    }
+    else
+        std::cout << "No solution found to place "
+                  << numberOfQueens
+                  << " queens on a "
+                  << numberOfQueens
+                  << " by "
+                  << numberOfQueens
+                  << " chess board"
+                  << std::endl;
 
     return 0;
 }
