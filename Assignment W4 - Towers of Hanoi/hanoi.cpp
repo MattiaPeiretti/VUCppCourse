791d144c583606631ff034ac2421b1db0612e605
Mattia Peiretti <mattia.peiretti@gmail.com>
2021-11-30 04:24:29 +0100

    Refactored W4

--- Assignment W4 - Towers of Hanoi/hanoi.cpp
+++ Assignment W4 - Towers of Hanoi/hanoi.cpp
@@ -1,7 +1,7 @@
 // ============================================
 //  Mattia Peiretti, November 2021
 //  https://mattiapeiretti.com/
-//  Assignment H4 - N of Queens
+//  Assignment W4 - Towers of Hanoi
 // ============================================
 
 #include <iostream>
@@ -11,34 +11,56 @@
 
 //------------------------
 
+enum
+{
+    pegA,
+    pegB,
+    pegC,
+};
+
 bool isValidOperation(std::vector<std::stack<int>> &station, int diskSize, int peg)
 {
-
     if (station.at(peg).empty())
         return 1;
     if (station.at(peg).top() > diskSize)
         return 1;
-
     return 0;
 }
 
+// Moves a disk between two pegs in the first possible direction.
 bool moveBetween(std::vector<std::stack<int>> &station, int peg1, int peg2)
 {
-
     std::vector<char> pegsNames{'A', 'B', 'C'};
 
     // moving from 1 to 2
     if (!station.at(peg1).empty() && isValidOperation(station, station.at(peg1).top(), peg2))
     {
-        std::cout << "Move disk " << station.at(peg1).top() << " from " << pegsNames.at(peg1) << " to " << pegsNames.at(peg2) << "." << std::endl;
+        std::cout << "Move disk "
+                  << station.at(peg1).top()
+                  << " from "
+                  << pegsNames.at(peg1)
+                  << " to "
+                  << pegsNames.at(peg2)
+                  << "."
+                  << std::endl;
+
         station.at(peg2).push(station.at(peg1).top());
         station.at(peg1).pop();
+
         return 1;
     }
     // moving from 2 to 1
     else if (!station.at(peg2).empty() && isValidOperation(station, station.at(peg2).top(), peg1))
     {
-        std::cout << "Move disk " << station.at(peg2).top() << " from " << pegsNames.at(peg2) << " to " << pegsNames.at(peg1) << "." << std::endl;
+        std::cout << "Move disk "
+                  << station.at(peg2).top()
+                  << " from "
+                  << pegsNames.at(peg2)
+                  << " to "
+                  << pegsNames.at(peg1)
+                  << "."
+                  << std::endl;
+
         station.at(peg1).push(station.at(peg2).top());
         station.at(peg2).pop();
 
@@ -50,22 +72,23 @@ bool moveBetween(std::vector<std::stack<int>> &station, int peg1, int peg2)
 
 bool solveHanoi(int NOfDisks, std::vector<std::stack<int>> &station, std::stack<int> endStack)
 {
-    if (station.at(2) == endStack)
+
+    if (station.at(pegC) == endStack)
         return 1;
 
     if (NOfDisks % 2 == 0)
     {
-        if (moveBetween(station, 0, 1))
-            if (moveBetween(station, 0, 2))
-                if (moveBetween(station, 1, 2))
+        if (moveBetween(station, pegA, pegB))
+            if (moveBetween(station, pegA, pegC))
+                if (moveBetween(station, pegB, pegC))
                     if (solveHanoi(NOfDisks, station, endStack))
                         return 1;
     }
     else
     {
-        if (moveBetween(station, 0, 2))
-            if (moveBetween(station, 0, 1))
-                if (moveBetween(station, 1, 2))
+        if (moveBetween(station, pegA, pegC))
+            if (moveBetween(station, pegA, pegB))
+                if (moveBetween(station, pegB, pegC))
                     if (solveHanoi(NOfDisks, station, endStack))
                         return 1;
     }
@@ -75,12 +98,11 @@ bool solveHanoi(int NOfDisks, std::vector<std::stack<int>> &station, std::stack<
 int main()
 {
     static const int NUMBER_OF_PEGS = 3;
-    static const int EMPTY_DISK_PLACE = 0;
 
+    std::vector<std::stack<int>> station(NUMBER_OF_PEGS);
     int numberOfDisks;
 
-    std::cout
-        << "How many disks should the tower consist of? ";
+    std::cout << "How many disks should the tower consist of? ";
     std::cin >> numberOfDisks;
 
     if (std::cin.fail() || numberOfDisks < 1)
@@ -89,13 +111,11 @@ int main()
         return 1;
     }
 
-    std::vector<std::stack<int>> station(NUMBER_OF_PEGS);
-
     for (int i = numberOfDisks; i >= 1; i--)
     {
-        station.at(0).push(i);
+        station.at(pegA).push(i);
     }
 
-    solveHanoi(numberOfDisks, station, station.at(0));
+    solveHanoi(numberOfDisks, station, station.at(pegA));
     return 0;
 }
