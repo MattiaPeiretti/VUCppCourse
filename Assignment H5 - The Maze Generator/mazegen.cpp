59eeafe522df6e3f30e5882547620e96ef6834be
MattiaPeiretti <mattia.peiretti@gmail.com>
2022-01-26 21:17:36 +0100

    Working version to polish

--- Assignment H5 - The Maze Generator/mazegen.cpp
+++ Assignment H5 - The Maze Generator/mazegen.cpp
@@ -18,8 +18,10 @@
 #include <stack>
 #include <cctype>
 #include <random>
+#include <tuple>
 #include <stdlib.h>
 
+// Random Number Generation engine setup
 using u32 = uint_least32_t;
 using engine = std::mt19937;
 
@@ -52,6 +54,21 @@ struct Coordinates
     }
 };
 
+Coordinates getCoordinatesFromDirection(Coordinates initialCoords, Directions direction)
+{
+    if (direction == top)
+        initialCoords.y--;
+    else if (direction == right)
+        initialCoords.x++;
+    else if (direction == bottom)
+        initialCoords.y++;
+    else if (direction == left)
+        initialCoords.x--;
+    else
+        return initialCoords; // Return initial coords if already in None state
+    return initialCoords;
+}
+
 class Cell
 {
 private:
@@ -60,18 +77,14 @@ private:
 
 public:
     Cell(){};
-    std::vector<Directions> getConnections();
+
+    std::vector<Directions> getConnections() { return connections; };
     bool setConnection(Directions direction);
     bool isConnectedTo(Directions direction);
-    bool setVisited();
-    bool resetVisited();
+
+    void setVisited() { _isVisited = true; };
     bool isVisited() { return _isVisited; }
-    void DEBUGPrintCellData()
-    {
-        LOGV(connections)
-        std::cout
-            << std::endl;
-    }
+    void resetVisited() { _isVisited = false; };
 };
 
 bool Cell::setConnection(Directions direction)
@@ -88,72 +101,54 @@ bool Cell::isConnectedTo(Directions direction)
     return (std::find(connections.begin(), connections.end(), direction) != connections.end());
 }
 
-bool Cell::setVisited()
-{
-    _isVisited = true;
-    return true;
-}
-
-bool Cell::resetVisited()
-{
-    _isVisited = false;
-    return true;
-}
-std::vector<Directions> Cell::getConnections()
-{
-    return connections;
-}
-
 // ========================
 
 class Maze
 {
 private:
-    std::vector<std::vector<Cell>> cells;
-    int sizeX;
-    int sizeY;
+    int sizeX, sizeY;
     std::vector<Coordinates> availableNeighbors;
+    std::vector<std::vector<Cell>> cells;
 
-    void printHorizontalLine(int length, int row, bool endline);
-    //-- Generation
-
+    // Generation
     std::stack<Coordinates> positionStack;
     std::uniform_int_distribution<u32> distribute2;
     std::uniform_int_distribution<u32> distribute3;
-    u32 seed;
     engine generator;
+
+    void printHorizontalLine(int length, int row, bool endline);
+
+    // Generation
+    bool generateWalls(Coordinates currentCoords, int visitedCount);
+    bool setCellConnection(int x, int y, Directions direction);
 
 public:
     Maze(int _sizeX, int _sizeY, int _seed);
+
     Cell &getCellByCoordinates(int x, int y);
-    bool setCellConnection(int x, int y, Directions direction);
+    Cell &getCellByCoordinates(Coordinates coord);
     void generateMaze();
     const void printMaze();
-    bool generateWalls(Coordinates currentCoords, int visitedCount);
-
-    void DEBUG()
-    {
-        int x, y;
-        x = y = 0;
-        for (auto &row : cells)
-        {
-            for (Cell &element : row)
-            {
-                std::cout << x << "," << y << std::endl;
-                element.DEBUGPrintCellData();
-                y++;
-            }
-            y = 0;
-            x++;
-        }
-    };
 };
+
+Maze::Maze(int _sizeX, int _sizeY, int _seed)
+{
+    cells = std::vector<std::vector<Cell>>(_sizeX, std::vector<Cell>(_sizeY));
+
+    sizeX = _sizeX;
+    sizeY = _sizeY;
+
+    generator = engine(_seed);
+    distribute2 = std::uniform_int_distribution<u32>(0, 1);
+    distribute3 = std::uniform_int_distribution<u32>(0, 2);
+}
 
 bool Maze::setCellConnection(int x, int y, Directions direction)
 {
     if (getCellByCoordinates(x, y).setConnection(direction) == false)
         return false;
 
+    // Add opposite connection to adjacent neighbors
     if (direction == top && y > 0)
         getCellByCoordinates(x, y - 1).setConnection(bottom);
     else if (direction == bottom && y < (sizeY - 1))
@@ -167,81 +162,56 @@ bool Maze::setCellConnection(int x, int y, Directions direction)
     return true;
 }
 
-// Maze::Maze(int _sizeX, int _sizeY) : cells(_sizeX, std::vector<Cell>(_sizeY))
-Maze::Maze(int _sizeX, int _sizeY, int _seed)
-{
-    cells = std::vector<std::vector<Cell>>(_sizeX, std::vector<Cell>(_sizeY));
-
-    sizeX = _sizeX;
-    sizeY = _sizeY;
-    // seed = _seed;
-
-    generator = engine(_seed);
-    distribute2 = std::uniform_int_distribution<u32>(0, 1);
-    distribute3 = std::uniform_int_distribution<u32>(0, 2);
-}
-
 void Maze::generateMaze()
 {
     static const Coordinates startCoords(0, 0);
     generateWalls(startCoords, 0);
-    for (auto &row : cells)
+    for (std::vector<Cell> &row : cells)
         for (Cell &element : row)
             element.resetVisited();
 }
 
 bool Maze::generateWalls(Coordinates currentCoords, int visitedCount)
 {
-    // Setting up random engine with the seed
-    //LOG((sizeX * sizeY) - 1);
     if (visitedCount >= ((sizeX * sizeY) - 1))
         return true;
 
-    // current cell visited
+    getCellByCoordinates(currentCoords).setVisited();
 
-    getCellByCoordinates(currentCoords.x, currentCoords.y).setVisited();
-
-    // check unvisited neighbors
     int availableNeighborsCount = 0;
     availableNeighbors = {};
+
     // -top
-    if ((currentCoords.y - 1) >= 0 && !(getCellByCoordinates(currentCoords.x, currentCoords.y - 1).isVisited()))
+    if ((currentCoords.y - 1) >= 0 && !(getCellByCoordinates(getCoordinatesFromDirection(currentCoords, top)).isVisited()))
     {
         availableNeighborsCount++;
-        //LOG("TOP")
         availableNeighbors.push_back(Coordinates(currentCoords.x, currentCoords.y - 1, top));
     }
     // -Right
-    if ((currentCoords.x + 1) < (sizeX) && !(getCellByCoordinates(currentCoords.x + 1, currentCoords.y).isVisited()))
+    if ((currentCoords.x + 1) < (sizeX) && !(getCellByCoordinates(getCoordinatesFromDirection(currentCoords, right)).isVisited()))
     {
         availableNeighborsCount++;
-        //LOG("RIGHT")
-
         availableNeighbors.push_back(Coordinates(currentCoords.x + 1, currentCoords.y, right));
     }
-
     // -bottom
-    if ((currentCoords.y + 1) < (sizeY) && !(getCellByCoordinates(currentCoords.x, currentCoords.y + 1).isVisited()))
+    if ((currentCoords.y + 1) < (sizeY) && !(getCellByCoordinates(getCoordinatesFromDirection(currentCoords, bottom)).isVisited()))
     {
         availableNeighborsCount++;
-        //LOG("BOTTOM")
-
         availableNeighbors.push_back(Coordinates(currentCoords.x, currentCoords.y + 1, bottom));
     }
     // -left
-    if ((currentCoords.x - 1) >= 0 && !(getCellByCoordinates(currentCoords.x - 1, currentCoords.y).isVisited()))
+    if ((currentCoords.x - 1) >= 0 && !(getCellByCoordinates(getCoordinatesFromDirection(currentCoords, left)).isVisited()))
     {
         availableNeighborsCount++;
-        //LOG("LEFT")
-
         availableNeighbors.push_back(Coordinates(currentCoords.x - 1, currentCoords.y, left));
     }
+
     // set cell connection
     Coordinates nextCellCoords;
+
     if (availableNeighborsCount == 1)
         nextCellCoords = availableNeighbors.at(0);
     else if (availableNeighborsCount == 2)
-        //LOG(distribute2(generator))
         nextCellCoords = availableNeighbors.at(distribute2(generator));
     else if (availableNeighborsCount == 3)
         nextCellCoords = availableNeighbors.at(distribute2(generator));
@@ -249,10 +219,7 @@ bool Maze::generateWalls(Coordinates currentCoords, int visitedCount)
     {
         positionStack.pop();
         if (generateWalls(positionStack.top(), visitedCount))
-        {
-
             return true;
-        }
     }
 
     visitedCount++;
@@ -293,7 +260,7 @@ const void Maze::printMaze()
 {
     const static std::string verticalWall = "|";
     const static std::string openVerticalWall = " ";
-    const static std::string filledCell = " O ";
+    const static std::string filledCell = " . ";
     const static std::string emptyCell = "   ";
     std::string cellContent;
     Cell currentCell;
@@ -331,22 +298,12 @@ Cell &Maze::getCellByCoordinates(int x, int y)
     return cells.at(x).at(y);
 }
 
-// ========================
-
-Coordinates getCoordinatesFromDirection(Coordinates initialCoords, Directions direction)
+Cell &Maze::getCellByCoordinates(Coordinates coord)
 {
-    if (direction == top)
-        initialCoords.y--;
-    else if (direction == right)
-        initialCoords.x++;
-    else if (direction == bottom)
-        initialCoords.y++;
-    else if (direction == left)
-        initialCoords.x--;
-    else
-        return initialCoords; // Return initial coords if already in None state
-    return initialCoords;
+    return cells.at(coord.x).at(coord.y);
 }
+
+// ========================
 
 bool findPath(Maze &m, Coordinates from, Coordinates to)
 {
@@ -394,19 +351,18 @@ int main(int argc, char *argv[])
     else
         seed = atoi(argv[3]);
 
-    sizeX = atoi(argv[1]);
-    sizeY = atoi(argv[2]);
-
-    // if (Coordinates(0, 0) == Coordinates(1, 0))
-    //     LOG("aaa");
+    sizeY = atoi(argv[1]);
+    sizeX = atoi(argv[2]);
 
     Maze m(sizeX, sizeY, seed);
 
     m.generateMaze();
-    std::cout
-        << std::endl;
-    if (findPath(m, Coordinates(0, 0), Coordinates(sizeX - 1, sizeY - 1)))
-        LOG("Yay!");
 
-    m.printMaze();
+    if (findPath(m, Coordinates(0, 0), Coordinates(sizeX - 1, sizeY - 1)))
+    {
+        m.printMaze();
+        return 0;
+    }
+
+    return -1;
 }
